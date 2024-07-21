d358774ed1a195971fe533f5976a899468bba458
Uriel Lozano <strike_ls@hotmail.com>
2024-07-21 00:13:45 -0600

    Menu navigation app

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  route:string,
+  name: string
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent implements OnInit {
+  public menuItems: MenuItem[] = [
+    {route: '/maps/fullscreen', name: 'Fullscreen'},
+    {route: '/maps/zoom-range', name: 'Zoom Range'},
+    {route: '/maps/markers', name: 'Markers'},
+    {route: '/maps/properties', name: 'Houses'},
+  ];
 
   constructor() { }
 
